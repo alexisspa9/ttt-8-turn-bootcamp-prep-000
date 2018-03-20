9c07f5ddcaaf43b3155afd96f3ab6328893b988a
Alexander Spanos <alexisspa9@gmail.com>
2018-03-20 10:55:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,15 @@ def display_board(board)
    puts "-----------"
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board, index)
+  if (index.between?(0-8) && board[index] == " ") 
+    return true
+  end
+end
+def input_to_index(input)
+end
+def move(board, index)
+  
+
+end
